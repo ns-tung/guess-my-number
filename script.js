855df1d1086c2005c1df8f843ba7be7ad6050154
Nguyen Son Tung <tungns.wd@gmail.com>
2024-03-20 16:15:03 +0700

    Init game's input

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+const score = document.querySelector('.score');
+const guess = document.querySelector('.guess');
+const check = document.querySelector('.check');
+const again = document.querySelector('.again');
+const highscore = document.querySelector('.highscore');
+
+message.textContent = '🥳 Correct Number';
+number.textContent = 20;
+score.textContent = 10;
+guess.value = 10;
