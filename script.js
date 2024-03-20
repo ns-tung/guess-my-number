41c2f09e916de7536decb0061be315c5e10df15f
Nguyen Son Tung <tungns.wd@gmail.com>
2024-03-20 17:14:03 +0700

    Implementing the game's logic

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 'use strict';
 
-const message = document.querySelector('.message');
-const number = document.querySelector('.number');
-const score = document.querySelector('.score');
-const guess = document.querySelector('.guess');
-const check = document.querySelector('.check');
-const again = document.querySelector('.again');
-const highscore = document.querySelector('.highscore');
+const messageEl = document.querySelector('.message');
+const numberEl = document.querySelector('.number');
+const scoreEl = document.querySelector('.score');
+const guessEl = document.querySelector('.guess');
+const checkEl = document.querySelector('.check');
+const againEl = document.querySelector('.again');
+const highScoreEl = document.querySelector('.highscore');
 
-message.textContent = '🥳 Correct Number';
-number.textContent = 20;
-score.textContent = 10;
-guess.value = 10;
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+checkEl.addEventListener('click', function () {
+  const guessNumber = Number(guessEl.value);
+  if (!guessNumber) {
+    messageEl.textContent = '🧐 Hmmm, no number!';
+  } else if (guessNumber === secretNumber) {
+    messageEl.textContent = '🥳 Correct Number!';
+  } else if (guessNumber > secretNumber) {
+    if (score > 1) {
+      score--;
+      scoreEl.textContent = score;
+      messageEl.textContent = '📈 Too hight!';
+    } else {
+      scoreEl.textContent = score = 0;
+      messageEl.textContent = '🤯 GAME OVER!'
+    }
+  } else if (guessNumber < secretNumber) {
+    if (score > 1) {
+      score--;
+      scoreEl.textContent = score;
+      messageEl.textContent = '📉 Too low!';
+    } else {
+      scoreEl.textContent = score = 0;
+      messageEl.textContent = '🤯 GAME OVER!'
+    }
+  }
+})
